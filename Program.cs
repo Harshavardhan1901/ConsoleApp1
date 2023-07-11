8e5ad8bf1852a9f8a5018856f567c2ac530bd86a
harsha1901@yahoo.com <harsha1901@yahoo.com>
2023-07-11 09:51:15 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Welcome to C#");
+            //Console.ReadKey();
+            //string firstName;
+            //string lastName;
+            //Console.WriteLine("Enter First Name" );
+            //firstName = Console.ReadLine();
+            //Console.WriteLine("Enter last Name");
+            //lastName = Console.ReadLine();
+            //Console.WriteLine("Welcome Mr.\\Ms. "+firstName+" "+lastName);
+            //Console.ReadKey();
+            //Ex 3
+            string name;
+            int age;
+            char grade;
+            DateTime doj;
+            double salary;
+            Console.WriteLine("Enter Name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Age");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Grade");
+            grade = Char.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Date Of Joining");
+            doj = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Salary");
+            salary = double.Parse(Console.ReadLine());
+            Console.WriteLine("Name : /t" + name + "\n Age: " + age + "\n Grade: \t" + grade + "\n Salary: \t "+salary +"\n Date Of Joining: \t "+doj);
+            Console.ReadKey();
+        }
+    }
+}
